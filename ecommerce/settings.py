59b93bc93ec7f48baf3a7cf62935c1b50a9f7f9f
Okooboh Efosa <okoobohefosa@gmail.com>
2020-10-18 07:05:39 +0100

    add changes

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -73,7 +73,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# location where heroku puts staticfiles
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
